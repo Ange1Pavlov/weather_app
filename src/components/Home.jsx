b8f0dc0c9b5241437266729ecb8d2f975a46ec79
Ange1Pavlov <118755755+Ange1Pavlov@users.noreply.github.com>
2024-05-14 10:00:08 +0300

    Added tailwind css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,17 +5,17 @@ import { useGlobalState } from './GlobalStateContext';
 
 const Home = () => {
   const { weatherData, forecastData } = useGlobalState();
-  
+
   if (!weatherData || !forecastData) {
     // Return a loading indicator or handle the case when data is not yet available
     return <div>Loading...</div>;
   }
 
   return (
-    <>
+    <div className='flex flex-col items-center justify-center'>
       <Weather data={weatherData} />
       <Forecast data={forecastData} />
-    </>
+    </div>
   );
 };
 
